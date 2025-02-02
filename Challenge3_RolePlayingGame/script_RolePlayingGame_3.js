7a689d62da34602617cfb6c903ef70d296bac653
sbu77blr <sbumsrit61blr@gmail.com>
2025-02-02 14:20:55 +0530

    Challenge3 _ v2

--- Challenge3_RolePlayingGame/script_RolePlayingGame_3.js
+++ Challenge3_RolePlayingGame/script_RolePlayingGame_3.js
@@ -10,3 +10,59 @@ let inventory = ["stick"];
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
+
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
+
+const locations = [];
+
+// Initializing the buttons.
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+
+// Function Definitions.
+let update = (location) => {
+    // Need to define function to update the buttons based on the current location.
+}
+
+let goTown = () => {
+    button1.innerText = "Go to store";
+    button2.innerText = "Go to cave";
+    button3.innerText = "Fight dragon";
+
+    button1.onclick = goStore;
+    button2.onclick = goCave;
+    button3.onclick = fightDragon;
+    text.innerText = "You are in the town square. You see a sign that says \"Store\".";
+}
+
+let goStore = function() {
+    button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
+
+    button1.onclick = buyHealth;
+    button2.onclick = buyWeapon;
+    button3.onclick = goTown;
+    text.innerText = "Your enter the store";
+}
+let goCave = function() {
+    console.log("Going to Cave");
+}
+let fightDragon = function() {
+    console.log("Fighting Dragon");
+}
+
+let buyHealth = () => {
+    // Logic yet to be defined.
+}
+let buyWeapon = () => {
+    // Logic yet to be defined.
+}
