387e06aac047631ba15e6f75c03f0d63571e915e
sbu77blr <sbumsrit61blr@gmail.com>
2025-02-05 23:50:20 +0530

    Challenge3 _ v4
    Halfway through the Monster and Fighting logic.

--- Challenge3_RolePlayingGame/script_RolePlayingGame_3.js
+++ Challenge3_RolePlayingGame/script_RolePlayingGame_3.js
@@ -7,6 +7,7 @@ let fighting;
 let monsterHealth;
 let inventory = ["stick"];
 
+// Element references.
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
@@ -21,8 +22,8 @@ const monsterHealthText = document.querySelector("#monsterHealth");
 
 
 // Function Definitions.
+// Function to update the buttons' text and action based on the current location.
 let update = (location) => {
-    // Need to define function to update the buttons based on the current location.
     button1.innerText = location["button text"][0];
     button2.innerText = location["button text"][1];
     button3.innerText = location["button text"][2];
@@ -44,9 +45,6 @@ let goStore = function() {
 let goCave = function() {
     update(locations[2]);
     // update(myLocations['cave']);
-}
-let fightDragon = function() {
-    console.log("Fighting Dragon");
 }
 
 let buyHealth = () => {
@@ -83,17 +81,66 @@ let buyWeapon = () => {
     }
     else{
         text.innerText = "You already have the most powerful weapon!";
+        button2.innerText = "Sell weapon for 15 gold";
+        button2.onclick = sellWeapon;
     }
 }
 
+let sellWeapon = () => {
+    // console.log("Selling weapon...");
+    // Player shld not be able to sell their 'only' weapon. So .. 
+    if(inventory.length > 1)
+    {
+        gold += 15;
+        goldText.innerText = gold;
+
+        let currentWeapon = inventory.shift();
+        text.innerText = `You sold a ${currentWeapon}.`;
+        text.innerText += ` In your inventory you have: ${inventory}`;
+    }
+    else {
+        text.innerText = "Don't sell your only weapon!";
+    }
+}
+
+// Common Fighting logic.
+let goFight = () => {
+    update(locations[3]);
+    // update(myLocations["fight"]);
+    
+    monsterHealth = monsters[fighting].health;
+    // monsterHealth = myMonsters[fighting].health;
+
+    monsterStats.style.display = "block";
+}
+let attack = () => {
+
+}
+
+let dodge = () => {
+
+}
+
 let fightSlime = () => {
-    // Logic yet to be defined.
-    console.log("Fighting Slime...");
+    // console.log("Fighting Slime...");
+    fighting = 0;
+    // fighting = "slime";
+    goFight();
 }
 
 let fightBeast = () => {
-    // Logic yet to be defined.
-    console.log("Fighting Beast...");
+
+    // console.log("Fighting Beast...");
+    fighting = 1;
+    // fighting = "fanged beast";
+    goFight();
+}
+
+let fightDragon = function() {
+    // console.log("Fighting Dragon");
+    fighting = 2;
+    // fighting = "dragon";
+    goFight();
 }
 
 // Initializing the buttons.
@@ -108,52 +155,79 @@ const weapons = [
     {name: "sword", power: 100}
 ];
 
+const monsters = [
+    {name: "slime", level: 2 , health: 15},
+    {name: "fanged beast", level: 8, health: 60},
+    {name: "dragon", level: 20, health: 300}
+];
+
 const locations = [
     {
-        name: "town square",
+        "name": "town square",
         "button text": ["Go to store", "Go to cave", "Fight dragon"],
         "button functions": [goStore, goCave, fightDragon],
-        text: "You are in the town square. You see a sign that says \"Store\"."
+        "text": "You are in the town square. You see a sign that says \"Store\"."
     },
     {
-        name: "store",
+        "name": "store",
         "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
         "button functions": [buyHealth, buyWeapon, goTown],
-        text: "You enter the store."
+        "text": "You enter the store."
     },
     {
-        name: "cave",
+        "name": "cave",
         "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
         "button functions": [fightSlime, fightBeast, goTown],
-        text: "You enter the cave. You see some monsters."
+        "text": "You enter the cave. You see some monsters."
     },
+    {
+        "name": "fight",
+        "button text": ["Attack", "Dodge", "Run"],
+        "button functions": [attack, dodge, goTown],
+        "text": "You are fighting a monster."
+    }
 ];
 
 /*
 // MyVersion : Object-based access.
 
 const myWeapons = {
-  "stick" : {power: 5},
-  "dagger" : {power: 30},
-  "claw hammer" : {power: 50},
-  "sword" : {power: 100}
+  "stick" : {name: "stick", power: 5},
+  "dagger" : {name: "dagger", power: 30},
+  "claw hammer" : {name: "claw hammer", power: 50},
+  "sword" : {name: "sword", power: 100}
+};
+
+const myMonsters = {
+    "slime" : {name: "slime", level: 2 , health: 15},
+    "fanged beast" : {name: "fanged beast", level: 8, health: 60},
+    "dragon" : {name: "dragon", level: 20, health: 300}
 };
 
 const myLocations = {
     "town square" : {
+        "name": "town square",
         "button text": ["Go to store", "Go to cave", "Fight dragon"],
         "button functions": [goStore, goCave, fightDragon],
         text: "You are in the town square. You see a sign that says \"Store\"."
     },
     "store" : {
+        "name": "store",
         "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
         "button functions": [buyHealth, buyWeapon, goTown],
         text: "You enter the store."
     },
     "cave" : {
+        "name": "cave",
         "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
         "button functions": [fightSlime, fightBeast, goTown],
         text: "You enter the cave. You see some monsters."
+    },
+    "fight" : {
+        "name": "fight",
+        "button text": ["Attack", "Dodge", "Run"],
+        "button functions": [attack, dodge, goTown],
+        "text": "You are fighting a monster."
     }
 };
 // */
