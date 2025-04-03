694c11cf97ef78ac3e6dfe1f89b1c64dd40f1bc8
Suvan77blr <sbumsrit61blr@gmail.com>
2025-04-03 21:16:07 +0530

    RPS-Game : Challenge6 DONE!
    All JS functions have been implemented and tested.
    Challenges_List has been updated.

--- Challenge6_RockPaperScissors-GAME/script_RPSGame_6.js
+++ Challenge6_RockPaperScissors-GAME/script_RPSGame_6.js
@@ -1,4 +1,21 @@
 
+let playerScore = 0;
+let computerScore = 0;
+
+const playerScoreSpanElement = document.getElementById("player-score");
+const computerScoreSpanElement = document.getElementById("computer-score");
+const roundResultsMsg = document.getElementById("results-msg");
+
+const winnerMsgElement = document.getElementById("winner-msg");
+winnerMsgElement.innerText = "";
+const optionsContainer = document.querySelector("#options-container");
+roundResultsMsg.innerText = "";
+
+// Button references.
+const resetGameBtn = document.getElementById("reset-game-btn");
+const rockBtn = document.getElementById("rock-btn");
+const paperBtn = document.getElementById("paper-btn");
+const scissorsBtn = document.getElementById("scissors-btn");
 
 let getRandomComputerResult = () => {
     const options = ["Rock", "Paper", "Scissors"];
@@ -7,33 +24,19 @@ let getRandomComputerResult = () => {
 };
 
 let hasPlayerWonTheRound = (player, computer) => {
-    // if(player=="Rock" && computer=="Scissors")
-    //     return true;
-    // else if(player=="Scissors" && computer=="Paper")
-    //     return true;
-    // else if(player=="Paper" && computer=="Rock")
-    //     return true;
-    // else return false;
 
     return (
-        (player=="Rock" && computer=="Scissors") ||
-        (player=="Scissors" && computer=="Paper") ||
-        (player=="Paper" && computer=="Rock")
+        (player === "Rock" && computer === "Scissors") ||
+        (player === "Scissors" && computer === "Paper") ||
+        (player === "Paper" && computer === "Rock")
     );
 };
 
 // console.log(hasPlayerWonTheRound("Rock", "Scissors")); 
 // console.log(hasPlayerWonTheRound("Scissors", "Rock")); 
 
-let playerScore = 0;
-let computerScore = 0;
-
 let getRoundResults = (userOption) => {
     const computerResult = getRandomComputerResult();
-
-    if(computerResult === userOption) {
-        return `It's a tie! Both chose ${userOption}`;
-    }
 
     const result = hasPlayerWonTheRound(userOption, computerResult);
 
@@ -41,12 +44,59 @@ let getRoundResults = (userOption) => {
         playerScore++;
         return `Player wins! ${userOption} beats ${computerResult}`;
     }
+    else if (computerResult === userOption) {
+        return `It's a tie! Both chose ${userOption}`;
+    }
     else {
         computerScore++;
         return `Computer wins! ${computerResult} beats ${userOption}`;
     }
 };
 
+// console.log(getRoundResults("Rock"));
+// console.log("Player Score: ", playerScore, "Computer Score: ", computerScore);
 
-console.log(getRoundResults("Rock"));
-console.log("Player Score: ", playerScore, "Computer Score: ", computerScore);
+
+let showResults = (userOption) => {
+    roundResultsMsg.innerText = getRoundResults(userOption);
+    computerScoreSpanElement.innerText = computerScore;
+    playerScoreSpanElement.innerText = playerScore;
+
+    if (playerScore === 3 || computerScore === 3) {
+        winnerMsgElement.innerText = `${
+            playerScore === 3 ? "Player" : "Computer"
+        } has won the game!`;
+
+        resetGameBtn.style.display = "block";
+        optionsContainer.style.display = "none";
+    }
+};
+
+let resetGame = () => {
+    playerScore = computerScore = 0;
+    playerScoreSpanElement.innerText = playerScore;
+    computerScoreSpanElement.innerText = computerScore;
+
+    winnerMsgElement.innerText = "";
+    roundResultsMsg.innerText = "";
+
+    resetGameBtn.style.display = "none";
+    optionsContainer.style.display = "block";
+}
+
+
+// resetGameBtn.addEventListener("click", resetGame);
+// rockBtn.addEventListener('click', showResults("Rock"));
+// paperBtn.addEventListener("click", showResults("Paper"));
+// scissorsBtn.addEventListener("click", showResults("Scissors"));
+
+resetGameBtn.addEventListener("click", resetGame);
+rockBtn.addEventListener('click', () => {
+    showResults("Rock")
+});
+paperBtn.addEventListener("click", () => {
+    showResults("Paper")
+});
+scissorsBtn.addEventListener("click", () => {
+    showResults("Scissors")
+});
